e5ce15d2046ddd763872ee3485bd99d729903af7
root <root@DESKTOP-1HVSU4I>
2024-11-05 17:52:38 +0530

    Code Started

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,25 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
@@ -10,14 +32,24 @@ pragma solidity ^0.8.18;
  */
 
 contract Raffle {
+    error Raffle__SendMoreToEnterRaffle();
     uint256 private immutable i_entranceFee;
+    address payable [] private s_palyers;
+
+    /**Events */
+    event RaffleEnter(address indexed player);
 
     constructor(uint256 entrancefee){
         i_entranceFee=entrancefee;
     }
 
     function enterRaffle() public payable{
-
+        // require(msg.value >=i_entranceFee,Raffle__SendMoreToEnterRaffle()); this is not suggestbale due to gas efficeint
+        if(msg.value < i_entranceFee){
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+        s_palyers.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender)
     }
 
     function pinkWinner() public{
