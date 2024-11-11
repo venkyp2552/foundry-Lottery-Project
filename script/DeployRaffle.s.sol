cd1eb8018aeb66d35329fe256a8698265df1c1c8
root <root@DESKTOP-1HVSU4I>
2024-11-11 17:48:15 +0530

    Creating pickWinner using VRF

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -1,14 +1,14 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
-import {Script} from "lib/forge-std/src/Script.sol";
-import {Raffle} from "../src/Raffle.sol";
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.18;
+// import {Script} from "lib/forge-std/src/Script.sol";
+// import {Raffle} from "../src/Raffle.sol";
 
-contract DeployRaffle is Script{
-    function run() external {
-        Raffle depluRaffle;
-        uint256 entranceFee = 0.1 ether;
-        vm.startBroadcast();
-        depluRaffle=new Raffle(entranceFee);
-        vm.stopBroadcast();
-    }
-}
+// contract DeployRaffle is Script{
+//     function run() external {
+//         Raffle depluRaffle;
+//         uint256 entranceFee = 0.1 ether;
+//         vm.startBroadcast();
+//         depluRaffle=new Raffle(entranceFee);
+//         vm.stopBroadcast();
+//     }
+// }
--- src/Raffle.sol
+++ src/Raffle.sol
@@ -19,45 +19,130 @@
 // private
 // view & pure functions
 
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-
+import {VRFConsumerBaseV2Plus} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 /**
  * @title A Sample Raffle Contract
  * @author Venkaiah P
- * @notice This contract is for creating simple Raflle 
+ * @notice This contract is for creating simple Raflle
  * @dev implements Chaninlink VRFv2.5
  */
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2Plus {
     error Raffle__SendMoreToEnterRaffle();
+    error Raffle__TransferFailed();
+    error Raffle__RaffleNotOpen();
+
+    /** Type Declarations */
+    enum RaffleState{
+        OPEN,
+        CALCULATING
+    }
+    /**State Variables */
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
     uint256 private immutable i_entranceFee;
-    address payable [] private s_palyers;
+    // @dev The Duration of lottery in seconds
+    uint256 private immutable i_interval;
+    //@dev we need lastTimeStamp which mean when we deploy the contract, we should take that time as start time of our lotter contract;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint256 private s_lastTimeStamp;
 
-    /**Events */
+    address payable[] private s_palyers;
+    address private s_recentWinner;
+    RaffleState private s_raffleState;
+
+    /** Events */
     event RaffleEnter(address indexed player);
-
-    constructor(uint256 entrancefee){
-        i_entranceFee=entrancefee;
+    event WinnerPicked(address indexed winner);
+    constructor(
+        uint256 entrancefee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
+        i_entranceFee = entrancefee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+        i_keyHash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        s_raffleState=RaffleState.OPEN;
+        // s_vrfCoordinator.requestRandomWords();
     }
 
-    function enterRaffle() public payable{
+    function enterRaffle() external payable {
         // require(msg.value >=i_entranceFee,Raffle__SendMoreToEnterRaffle()); this is not suggestbale due to gas efficeint
-        if(msg.value < i_entranceFee){
+        if (msg.value < i_entranceFee) {
             revert Raffle__SendMoreToEnterRaffle();
         }
+        if(s_raffleState != RaffleState.OPEN){
+            revert Raffle__RaffleNotOpen();
+        }
         s_palyers.push(payable(msg.sender));
-        emit RaffleEnter(msg.sender)
+        emit RaffleEnter(msg.sender);
     }
 
-    function pinkWinner() public{
+    //1.Get a Random Number
+    //2.Use Random Number to pick a player
+    //3.Be automated call.
 
+    function pinkWinner() external {
+        //Checks
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+
+        //Interactions (External Contracts Interactions)
+        s_raffleState=RaffleState.CALCULATING;
+         VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({
+            keyHash: i_keyHash,
+            subId: i_subscriptionId,
+            requestConfirmations: REQUEST_CONFIRMATIONS,
+            callbackGasLimit: i_callbackGasLimit,
+            numWords: NUM_WORDS,
+            extraArgs: VRFV2PlusClient._argsToBytes(
+                // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+            )
+        });
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
     }
 
-    /**Getter Functions */
-    function getEntranceFee() external view returns(uint256){
+    /**
+     * Getter Functions
+     */
+    function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
+    }
+
+    //CEI => Checks, Effects, Interactions
+
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {
+        // Checks
+
+        //we need only one random number bcz NUM_WORDS we declare as 1 only.
+        // So from here also randomWords[0]  we should get only one index number
+
+        //Effecte (Internal Contract State)
+        uint256 indexOfWinner=randomWords[0] % s_palyers.length;
+        address payable recentWinner=s_palyers[indexOfWinner];
+        s_recentWinner=recentWinner;
+        s_raffleState=RaffleState.OPEN;
+        s_palyers=new address payable[](0);
+        s_lastTimeStamp=block.timestamp;
+        emit WinnerPicked(s_recentWinner);
+        //Interactions (External Contract Interactions)
+        (bool success,)=recentWinner.call{value:address(this).balance}("");
+        if(!success){
+            revert Raffle__TransferFailed();
+        }
     }
 }
